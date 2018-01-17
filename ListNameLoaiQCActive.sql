a3c289ef478c1c57f75ea5b11f76d0439ef12627
Hoang Pham Truc Phuong (CME.MSA) <hptphuong@gmail.com>
2018-01-17 13:54:00 +0700

    update store

--- ListNameLoaiQCActive.sql
+++ ListNameLoaiQCActive.sql
@@ -3,11 +3,11 @@ DROP procedure IF EXISTS `ListNameLoaiQCActive`;
 
 DELIMITER $$
 USE `asdgo_cms`$$
-CREATE PROCEDURE `ListNameLoaiQCActive` ()
+CREATE PROCEDURE `ListNameLoaiQCActive`()
 BEGIN
 	select id, ten_loai_quang_cao
-    from loai_quang_cao
-    where stasus_record=1;
+    from asdgo_cms.loai_quang_cao
+    where status_record=1;
 END$$
 
 DELIMITER ;
